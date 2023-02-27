9bfa340ff665ede4b25fecd5f9353178977074a0
Kabotei <50360652+Kabotei@users.noreply.github.com>
2023-02-27 13:29:40 -0500

    focal_loss

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -381,6 +381,50 @@ class FastRCNNOutputLayers(nn.Module):
             fed_loss_classes = unique_gt_classes
         return fed_loss_classes
 
+    def sigmoid_focal_loss(
+        inputs: torch.Tensor,
+        targets: torch.Tensor,
+        alpha: float = -0.25,
+        gamma: float = 2,
+        reduction: str = "none",
+    ) -> torch.Tensor:
+        """
+        Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+        Args:
+            inputs: A float tensor of arbitrary shape.
+                    The predictions for each example.
+            targets: A float tensor with the same shape as inputs. Stores the binary
+                    classification label for each element in inputs
+                    (0 for the negative class and 1 for the positive class).
+            alpha: (optional) Weighting factor in range (0,1) to balance
+                    positive vs negative examples. Default = -1 (no weighting).
+            gamma: Exponent of the modulating factor (1 - p_t) to
+                balance easy vs hard examples.
+            reduction: 'none' | 'mean' | 'sum'
+                    'none': No reduction will be applied to the output.
+                    'mean': The output will be averaged.
+                    'sum': The output will be summed.
+        Returns:
+            Loss tensor with the reduction option applied.
+        """
+        inputs = inputs.float()
+        targets = targets.float()
+        p = torch.sigmoid(inputs)
+        ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+        p_t = p * targets + (1 - p) * (1 - targets)
+        loss = ce_loss * ((1 - p_t) ** gamma)
+
+        if alpha >= 0:
+            alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+            loss = alpha_t * loss
+
+        if reduction == "mean":
+            loss = loss.mean()
+        elif reduction == "sum":
+            loss = loss.sum()
+
+        return loss
+    
     # Implementation from https://github.com/xingyizhou/CenterNet2/blob/master/projects/CenterNet2/centernet/modeling/roi_heads/custom_fast_rcnn.py#L113  # noqa
     # with slight modifications
     def sigmoid_cross_entropy_loss(self, pred_class_logits, gt_classes):
@@ -400,9 +444,8 @@ class FastRCNNOutputLayers(nn.Module):
         target[range(len(gt_classes)), gt_classes] = 1
         target = target[:, :K]
 
-        cls_loss = F.binary_cross_entropy_with_logits(
-            pred_class_logits[:, :-1], target, reduction="none"
-        )
+        #cls_loss = F.binary_cross_entropy_with_logits(pred_class_logits[:, :-1], target, reduction="none")
+        cls_loss = self.sigmoid_focal_loss(pred_class_logits[:, :-1], target, alpha = -0.25, gamma = 2, reduction = "none")
 
         if self.use_fed_loss:
             fed_loss_classes = self.get_fed_loss_classes(
